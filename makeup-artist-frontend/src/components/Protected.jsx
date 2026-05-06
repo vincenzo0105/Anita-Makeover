f656e453b7f6bc58ef49dcd064d2b11531c6f4be
vincenzo0105 <omk145593@gmail.com>
2026-05-06 19:21:54 +0530

    security - 10s test

--- makeup-artist-frontend/src/components/Protected.jsx
+++ makeup-artist-frontend/src/components/Protected.jsx
@@ -2,23 +2,32 @@ import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
 export default function Protected({ children }) {
+
   const token = localStorage.getItem("token");
 
+  // No token
   if (!token) {
     return <Navigate to="/admin/login" replace />;
   }
 
   try {
+
     const decoded = jwtDecode(token);
 
-    // Expired token
+    // Token expired
     if (decoded.exp * 1000 < Date.now()) {
+
       localStorage.removeItem("token");
+
       return <Navigate to="/admin/login" replace />;
     }
 
   } catch (error) {
+
+    console.error("Invalid token:", error);
+
     localStorage.removeItem("token");
+
     return <Navigate to="/admin/login" replace />;
   }
 
